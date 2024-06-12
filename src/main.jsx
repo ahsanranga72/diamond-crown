e30bbf737d8a685e187bd0fb27a99df291ebd81c
ahsanranga72 <ahsan.ranga@gmail.com>
2024-06-12 20:33:19 +0600

    nav and carousel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/style.css'
+import './assets/lib/animate/animate.min.css'
+import './assets/lib/owlcarousel/assets/owl.carousel.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
